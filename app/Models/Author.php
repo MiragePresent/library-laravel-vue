e2f8cb1ba4c5e7065a85e940689e6f28cd55f6eb
David Holovii <mirage.present@gmail.com>
2018-04-16 20:08:40 +0300

    Author model and other retated stuff

---
+++ app/Models/Author.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ *  Author model
+ *
+ * @property int $id
+ * @property string $name
+ * @property-read  string $short_name Name in format Surname N. (N => Name)
+ *
+ */
+
+class Author extends Model
+{
+
+    protected $fillable = [
+        'name'
+    ];
+
+    // ACCESSORS
+
+    /**
+     * @param $name
+     * @return string
+     */
+    public function getShortNameAttribute($name)
+    {
+        return collect(explode(' ', $name))->map(function ($name_part, $index) {
+            return !$index ?  $name_part : strtoupper($name_part{0});
+        })->implode(' ');
+    }
+}
